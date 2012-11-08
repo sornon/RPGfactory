45fcd6707134088c977076b3bff07e41babb5479
SORNON <sornon0719@gmail.com>
2012-11-09 01:06:36 +0800

    item ex

---
+++ js/items.js
@@ -0,0 +1,15 @@
+var items = {
+	armor:[],
+	arm:[],
+	shoulder:[],
+	hand:[],
+	waist:[],
+	leg:[],
+	foot:[],
+	weapon:[],
+	cloak:[],
+	ring:[],
+	neck:[],
+	shield:[]
+	
+}
---
+++ js/nude.js
@@ -0,0 +1,13 @@
+var nude = {
+	armor:[],
+	arm:[],
+	shoulder:[],
+	hand:[],
+	waist:[],
+	leg:[],
+	foot:[],
+	head:[],
+	hair:[],
+	face:[]
+	
+}
